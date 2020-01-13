a7a6779b1d362f2e82bdf431cb062efe6c6ed2a9
maciexu <43970987+maciexu@users.noreply.github.com>
2020-01-13 10:46:45 -0500

    Add files via upload

---
+++ First_Day.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan 10 19:20:12 2020
+
+@author: mengx
+"""
+
+"""
+First_Day
+"""
+
+print(2.0+8.)
+
+y=200
+print(y+1)
+# Ctrl+1 to option out exe
+
+
+print(2==2)
+
+print('a string '*2)
+
+number_of_dog=3
+print(number_of_dog*3)
+
+first, second, third=1,2,3
+
+A=2
+B=3
+print(A==B)
+print(A!=B)
+
