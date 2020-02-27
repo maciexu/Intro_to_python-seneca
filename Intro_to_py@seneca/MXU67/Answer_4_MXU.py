5f12d952a8fb7d7cbd5bd2c6d021c79591320f3e
maciexu <mengxixu@trentu.ca>
2020-02-27 16:10:25 -0500

    Q4 updates

--- Intro_to_py@seneca/MXU67/Answer_4_MXU.py
+++ Intro_to_py@seneca/MXU67/Answer_4_MXU.py
@@ -10,19 +10,36 @@ data = []
 json_file = open('products.json', encoding = 'utf8')
 json_str = json_file.read()
 json_data = json.loads(json_str)
-#for item in range(len(json_data)):
-#    print(json_data[item]["name"])
-
+dlst = []
+for i in range(len(json_data)):
+    name = json_data[i]["name"]
+    if name is not None:
+       fir_sec = name.split(' - ', maxsplit=1)
+       if len(fir_sec) == 1:
+           first = fir_sec[0]
+           second = ""
+           dic = {first: second}
+       elif len(fir_sec) > 1:
+           first = fir_sec[0]
+           second = fir_sec[1]
+           dic = {first: second}
+       dlst.append(dic)       
+#print(dlst)
+    
 from collections import defaultdict
 d = defaultdict(list)
 
-for k, v in json_data.items():
-    d[k].append(v)
-
-print(d)
+for myd in dlst:
+    for k, v in myd.items():
+        d[k].append(v)
+#print(d)
     
-
-
+user = input("plz enter a product name:")
+if user not in d.keys():
+    print(f"{user} NOT found.")
+elif user in d.keys():
+    for x in d[user]:
+        print(x)
 
 
 
