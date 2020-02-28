5ceb6b96fcf0831ddca33183d09bc275b3953a22
maciexu <mengxixu@trentu.ca>
2020-02-28 12:43:02 -0500

    Q1 update2

--- Intro_to_py@seneca/MXU67/Answer_1_MXU.py
+++ Intro_to_py@seneca/MXU67/Answer_1_MXU.py
@@ -7,23 +7,33 @@ lst[3]=3
 example_lst = [-2,0,3,2,6,9,7]
 print(f"example_list is {example_lst}.")
 lst1 = sorted(example_lst)
-print("The number that its index is equal to the item is:")
+lst2 = []
+
 for i in range(len(lst1)):
     if i == lst1[i]:
-        print(i)
+       lst2.append(i)
+print(f"The number that its index is equal to the item is: {lst2}")
 
 
-
-user = input("please enter a number (Attention: number only!). If you finish you entering, plz enter OK :")
+user = input("please enter a number (Attention: number only! At least one number!). If you finish you entering, plz enter OK :")
 lst = []
 while user != "OK":      
       lst.append(int(user))
       user = input("please enter a number. If you finish you entering, plz enter OK :")     
-print(f"This is your list: {lst}.")          
+print(f"This is your original list: {lst}.")          
 
 lst1 = sorted(lst)
 
-print("The number that its index is equal to the item is:")
+lst2 = []
+
 for i in range(len(lst1)):
     if i == lst1[i]:
-        print(i)
+       lst2.append(i)
+       
+if bool(lst2) == False:
+   print(f"In your sorted list {lst1}, there's no number equal to its index.")       
+elif bool(lst2) == True:
+   print(f"In your sorted list {lst1}, some the number(s) equal to its index : {lst2}")        
+        
+        
+        
