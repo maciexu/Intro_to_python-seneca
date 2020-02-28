4b3fc656abc4900f5ceec25bd88a3beb76313b15
maciexu <mengxixu@trentu.ca>
2020-02-28 12:16:08 -0500

    Q1 updates

--- Intro_to_py@seneca/MXU67/Answer_1_MXU.py
+++ Intro_to_py@seneca/MXU67/Answer_1_MXU.py
@@ -4,11 +4,26 @@ index. For example if the list is lst= [-2,0,2,3,6,7,9] then code has to return 
 lst[3]=3
 """
 
-lst = [-2,0,3,2,6,9,7]
-lst1 = sorted(lst)
-print(lst)
-print(lst1)
+example_lst = [-2,0,3,2,6,9,7]
+print(f"example_list is {example_lst}.")
+lst1 = sorted(example_lst)
+print("The number that its index is equal to the item is:")
+for i in range(len(lst1)):
+    if i == lst1[i]:
+        print(i)
 
+
+
+user = input("please enter a number (Attention: number only!). If you finish you entering, plz enter OK :")
+lst = []
+while user != "OK":      
+      lst.append(int(user))
+      user = input("please enter a number. If you finish you entering, plz enter OK :")     
+print(f"This is your list: {lst}.")          
+
+lst1 = sorted(lst)
+
+print("The number that its index is equal to the item is:")
 for i in range(len(lst1)):
     if i == lst1[i]:
         print(i)
