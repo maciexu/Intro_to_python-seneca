dd92daed4556adaf2b9341c6f89b2da0f6c0e8d5
maciexu <mengxixu@trentu.ca>
2020-02-28 16:50:32 -0500

    Q3 update

--- Intro_to_py@seneca/MXU67/Answer_3_MXU.py
+++ Intro_to_py@seneca/MXU67/Answer_3_MXU.py
@@ -5,7 +5,9 @@ Created on Wed Feb 26 00:38:24 2020
 @author: mengx
 """
 
-lst = [3,4,1,2,9]
+lst = [3,4,1,2,9,8, 0,10]
+print(f"This is a given list. {lst}")
+
 k = []
 v = []
 
@@ -16,13 +18,16 @@ for i in range(len(lst)):
     
 dd = dict(zip(k,v))
 
+ab = []
+
 for key, value in dd.items():
     if (10 - int(key)) in value:
-        a = int(key)
-        b = 10 - a
+        two = int(key), 10 - int(key)
+        ab.append(two)
         
-if a is not None:
-    print(f"there'r two numbers that can add up to 10. They'r {a} and {b}.")
+        
+if ab is not None:
+    print(f"there'r two numbers that can add up to 10. They'r {ab}.")
 else:
     print("there'r NO two numbers that can add up to 10.")
 
