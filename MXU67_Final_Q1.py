17f922431123a9b1468f1385061a67738ecc462e
maciexu <43970987+maciexu@users.noreply.github.com>
2020-04-14 23:51:28 -0400

    Update MXU67_Final_Q1.py

--- MXU67_Final_Q1.py
+++ MXU67_Final_Q1.py
@@ -30,13 +30,24 @@ dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
 print("date and time =", dt_string)
 
 """
+print("===INSTRUCTION=== \n \n")
+print("First Stage Input:")
+print("User will define the number of cache. And the original cashe.")
+print("First Stage Output: \nTo show the user the origal cache with timestamp.\n")
+print("Second Stage Input:")
+print("User will update their cache if Y is entered.")
+print("Second Stage Output: \nThe deleted LRU cache and the updated cache with timestamp.")
+ 
+
+
 from datetime import datetime
 from operator import itemgetter
 
 size = int(input("Plz determine how many website you wanna save: "))
 
+webs = []
 def fav():
-    web = input("Plz enter your favorate website: ")
+    web = input(f"Plz enter your favorate {size} website: ").upper()
     now = datetime.now()
     now = now.strftime("%d/%m/%Y %H:%M:%S")
     return dict(name = web,
@@ -55,17 +66,19 @@ for item in mycache:
     print(f"{web} was added at {added}.")
     
 
-def new_fav():
-    web = input("What is the latest website you visited?: ")
+def new_fav(web):
+    web = input("What is the latest website you visited?: ").upper()
     now = datetime.now()
     now = now.strftime("%d/%m/%Y %H:%M:%S")
     global mycache
+    global webs
     if web in webs:
         print("your favorate webs site are the same.")
     else:
         mycache.append(dict(name = web, updated = now))
         mycache = sorted(mycache,key = itemgetter("updated"),reverse=True)
         lru_web = mycache.pop()["name"]
+        webs = [dic["name"]for dic in mycache]
         print(f"The least recently used website is: {lru_web}, and it's been deleted.")
         print(f"These are your {size} favorate wbesites: ")
         for item in mycache:
@@ -74,10 +87,9 @@ def new_fav():
             print(f"{web} was added at {added}.")
         return mycache
   
-user = "Y"
+user = input("Do you wanna continue updating? Y or enter any other key to stop: ").upper()
+    
 while user == "Y":
-    new_fav()
-    user = input("Do you wanna continue updating your favorate webiste(s)? Y/N: ")
-
-
+    new_fav(web)
+    user = input("Do you wanna continue updating? Y or enter any other key to stop: ").upper()
 
